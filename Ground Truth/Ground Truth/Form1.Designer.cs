3def2bca98cef0817ad14021706264ba55718afb
Lucasjrt <lucasjrt@outlook.com>
2018-06-09 19:25:28 -0300

    Implementação da interface

--- Ground Truth/Ground Truth/Form1.Designer.cs
+++ Ground Truth/Ground Truth/Form1.Designer.cs
@@ -28,59 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnSalvar = new System.Windows.Forms.Button();
             this.btnZoomIn = new System.Windows.Forms.Button();
             this.btnZoomOut = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnProcurar = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.picImagem = new System.Windows.Forms.PictureBox();
+            this.txtTamanho = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSalvar
             // 
-            this.btnSalvar.Location = new System.Drawing.Point(762, 424);
+            this.btnSalvar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSalvar.Enabled = false;
+            this.btnSalvar.Location = new System.Drawing.Point(720, 504);
             this.btnSalvar.Name = "btnSalvar";
-            this.btnSalvar.Size = new System.Drawing.Size(75, 36);
+            this.btnSalvar.Size = new System.Drawing.Size(75, 30);
             this.btnSalvar.TabIndex = 0;
             this.btnSalvar.Text = "Salvar";
             this.btnSalvar.UseVisualStyleBackColor = true;
             // 
             // btnZoomIn
             // 
-            this.btnZoomIn.Location = new System.Drawing.Point(663, 424);
+            this.btnZoomIn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnZoomIn.Enabled = false;
+            this.btnZoomIn.Location = new System.Drawing.Point(670, 504);
             this.btnZoomIn.Name = "btnZoomIn";
-            this.btnZoomIn.Size = new System.Drawing.Size(45, 36);
+            this.btnZoomIn.Size = new System.Drawing.Size(45, 30);
             this.btnZoomIn.TabIndex = 1;
             this.btnZoomIn.Text = "+";
             this.btnZoomIn.UseVisualStyleBackColor = true;
+            this.btnZoomIn.Click += new System.EventHandler(this.btnZoomIn_Click);
             // 
             // btnZoomOut
             // 
-            this.btnZoomOut.Location = new System.Drawing.Point(612, 424);
+            this.btnZoomOut.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnZoomOut.Enabled = false;
+            this.btnZoomOut.Location = new System.Drawing.Point(575, 504);
             this.btnZoomOut.Name = "btnZoomOut";
-            this.btnZoomOut.Size = new System.Drawing.Size(45, 36);
+            this.btnZoomOut.Size = new System.Drawing.Size(45, 30);
             this.btnZoomOut.TabIndex = 2;
             this.btnZoomOut.Text = "-";
             this.btnZoomOut.UseVisualStyleBackColor = true;
+            this.btnZoomOut.Click += new System.EventHandler(this.btnZoomOut_Click);
             // 
-            // textBox1
+            // txtFile
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(21, 424);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(395, 34);
-            this.textBox1.TabIndex = 3;
+            this.txtFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.txtFile.Location = new System.Drawing.Point(12, 504);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtFile.Size = new System.Drawing.Size(395, 28);
+            this.txtFile.TabIndex = 3;
+            this.txtFile.Text = "C:\\";
+            // 
+            // btnProcurar
+            // 
+            this.btnProcurar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnProcurar.Location = new System.Drawing.Point(413, 504);
+            this.btnProcurar.Name = "btnProcurar";
+            this.btnProcurar.Size = new System.Drawing.Size(41, 28);
+            this.btnProcurar.TabIndex = 4;
+            this.btnProcurar.Text = "...";
+            this.btnProcurar.UseVisualStyleBackColor = true;
+            this.btnProcurar.Click += new System.EventHandler(this.btnProcurar_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Imagem JPG| *.jpg";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAbrir.Enabled = false;
+            this.btnAbrir.Location = new System.Drawing.Point(460, 504);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(60, 28);
+            this.btnAbrir.TabIndex = 5;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // picImagem
+            // 
+            this.picImagem.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picImagem.Location = new System.Drawing.Point(12, 12);
+            this.picImagem.Name = "picImagem";
+            this.picImagem.Size = new System.Drawing.Size(783, 465);
+            this.picImagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picImagem.TabIndex = 6;
+            this.picImagem.TabStop = false;
+            this.picImagem.Click += new System.EventHandler(this.picImagem_Click);
+            // 
+            // txtTamanho
+            // 
+            this.txtTamanho.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTamanho.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            this.txtTamanho.Location = new System.Drawing.Point(625, 507);
+            this.txtTamanho.Name = "txtTamanho";
+            this.txtTamanho.Size = new System.Drawing.Size(40, 22);
+            this.txtTamanho.TabIndex = 7;
+            this.txtTamanho.Text = "25";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(869, 501);
-            this.Controls.Add(this.textBox1);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(807, 544);
+            this.Controls.Add(this.txtTamanho);
+            this.Controls.Add(this.picImagem);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.btnProcurar);
+            this.Controls.Add(this.txtFile);
             this.Controls.Add(this.btnZoomOut);
             this.Controls.Add(this.btnZoomIn);
             this.Controls.Add(this.btnSalvar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ground Truth";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,9 +166,14 @@
         #endregion
 
         private System.Windows.Forms.Button btnSalvar;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtFile;
         private System.Windows.Forms.Button btnZoomOut;
         private System.Windows.Forms.Button btnZoomIn;
+        private System.Windows.Forms.Button btnProcurar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.PictureBox picImagem;
+        private System.Windows.Forms.TextBox txtTamanho;
     }
 }
 
--- Ground Truth/Ground Truth/Form1.cs
+++ Ground Truth/Ground Truth/Form1.cs
@@ -12,6 +12,8 @@ namespace Ground_Truth
 {
     public partial class Form1 : Form
     {
+        Image imagemOriginal;
+
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +22,42 @@ namespace Ground_Truth
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        string file;
+
+        private void btnProcurar_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                file = openFileDialog1.FileName;
+                imagemOriginal = Image.FromFile(file);
+                txtFile.Text = file;
+                btnAbrir.Enabled = true;
+            }
+        }
+
+        private void picImagem_Click(object sender, EventArgs e) {}
+
+        private void btnAbrir_Click(object sender, EventArgs e) {
+            picImagem.Image = imagemOriginal;
+            btnZoomIn.Enabled = true;
+            btnZoomOut.Enabled = true;
+        }
+
+        private void btnZoomIn_Click(object sender, EventArgs e) {
+            if(Convert.ToInt32(txtTamanho.Text) < picImagem.Image.Height && Convert.ToInt32(txtTamanho.Text) < picImagem.Image.Width)
+               txtTamanho.Text =Convert.ToString(Convert.ToInt32(txtTamanho.Text) + 1);
+        }
+
+        private void btnZoomOut_Click(object sender, EventArgs e) {
+            if(Convert.ToInt32(txtTamanho.Text) > 1)
+                txtTamanho.Text = Convert.ToString(Convert.ToInt32(txtTamanho.Text) - 1);
         }
     }
 }
